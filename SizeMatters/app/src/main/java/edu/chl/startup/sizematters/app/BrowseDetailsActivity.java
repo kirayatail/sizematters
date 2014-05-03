e69d6ba079f49d2fe175a8f821b8345b98cb2c29
Max Witt <kirayatail@gmail.com>
2014-05-03 14:14:37 +0200

    Created BrowseDetailsActivity, which displays all saved measurements and objects

---
+++ SizeMatters/app/src/main/java/edu/chl/startup/sizematters/app/BrowseDetailsActivity.java
@@ -0,0 +1,16 @@
+package edu.chl.startup.sizematters.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by max on 2014-05-03.
+ */
+public class BrowseDetailsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browsedetailsactivitylayout);
+    }
+
+    
+}
